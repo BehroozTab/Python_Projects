67bc31c1833877baa1b269bd735f2a961f1f0f6b
Behrooz Tabrizian <t_behrooz2000@yahoo.com>
2019-01-24 11:13:57 -0500

    Add files via upload

---
+++ Create_Table.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "CREATE TABLE dept(deptid INT(2) not null," 
+sql += "DeptName VARCHAR(15), Location VARCHAR(15),"
+sql += "PRIMARY KEY (deptid))"
+mycursor.execute(sql)
+
+sql = "CREATE TABLE employee" 
+sql += "(Employeeld INT(3) not null, Lname VARCHAR(20), Fname VARCHAR(20),"
+sql += "Salary DOUBLE(9,2), deptid INT(2) not null,"
+sql += "PRIMARY KEY (Employeeld),"
+sql += "FOREIGN KEY (deptid) REFERENCES dept(deptid))"
+mycursor.execute(sql)
+
+
