b33d73d6493dd933e78303507a30f01e7eed7eca
Behrooz Tabrizian <t_behrooz2000@yahoo.com>
2019-05-03 19:26:38 -0400

    Update Insert_Table.py

--- Mysql_Connector/Insert_Table.py
+++ Mysql_Connector/Insert_Table.py
@@ -6,3 +6,27 @@ mydb = mysql.connector.connect(
   passwd="",
   database="mydatabase"
 )
+
+mcursor = mydb.cursor()
+
+sql = "INSERT INTO dept (deptid, DeptName, Location) VALUES (%s, %s, %s)"
+val = [
+  (1, 'Human Resources', 'st. remi'),
+  (2, 'Accounting', 'st. remi'),
+  (3, 'Computer', 'st. remi')
+]
+
+try:
+ mcursor.executemany(sql, val)
+except:
+  print("The departments exist")
+
+mydb.commit()
+
+sql = "SELECT * from dept"
+mcursor.execute(sql)
+mresult = mcursor.fetchall()
+
+for x in mresult:
+  print("department ID: ",x[0] , "\t\tdepartment Name: ", x[1] , "\t\tdepartment Location: ", x[2])
+  
