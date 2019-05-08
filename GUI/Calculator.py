beecc7c6b4cc0449dbfc2db40814ba5a6fd2ae92
Behrooz Tabrizian <t_behrooz2000@yahoo.com>
2019-05-08 12:17:30 -0400

    Update Calculator.py

--- GUI/Calculator.py
+++ GUI/Calculator.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 win = tk.Tk()
-win.title("Hello")
+win.title("Calculator")
 
 con1 = tk.IntVar()
-opr = tk.StringVar()
-num1=tk.IntVar()
+opr = ''
+num1 = tk.IntVar()
+
 frame = tk.Frame(win)
 frame.grid(row=2, column=5)
 
@@ -13,25 +14,48 @@ def addnum(k):
   v = con1.get()
   con1.set(v*10+k)
 
-def savenum():
+def savenum(op):
   global con1
   global num1
+  global opr
+  opr = op
   num1 = con1.get()
   con1.set(0)
 
-l1 = tk.Label(frame,width=20,textvariable = con1)
+def calc():
+  global con1
+  global num1
+  global opr
+  n1 = num1
+  n2 = con1.get()
+  if opr=='+':
+    con1.set(n1+n2)
+
+l1 = tk.Label(frame,width=15, height=2,bg = "white",textvariable = con1)
 l1.grid(row=0, column=1)
+#l1.pack(side="right")
 l2 = tk.Label(frame, text = "Exp :")
 l2.grid(row=0, column=0)
 
-b1 = tk.Button(frame, text = "  1  ",width=15,height=2,command=(lambda con1=1: addnum(con1)))
-b1.grid(row=2, column=1)
-b2 = tk.Button(frame, text = "  2  ",width=15,height=2,command=(lambda con1=2: addnum(con1)))
-b2.grid(row=2, column=0)
+
+b1 = tk.Button(frame, text = "  1  ",width=15,height=2,command=(lambda n=1: addnum(n)))
+b1.grid(row=2, column=2)
+b2 = tk.Button(frame, text = "  2  ",width=15,height=2,command=(lambda n=2: addnum(n)))
+b2.grid(row=2, column=1)
+b3 = tk.Button(frame, text = "  3  ",width=15,height=2,command=(lambda n=3: addnum(n)))
+b3.grid(row=2, column=0)
+b4 = tk.Button(frame, text = "  4  ",width=15,height=2,command=(lambda n=4: addnum(n)))
+b4.grid(row=3, column=2)
+b5 = tk.Button(frame, text = "  5  ",width=15,height=2,command=(lambda n=5: addnum(n)))
+b5.grid(row=3, column=1)
+b6 = tk.Button(frame, text = "  6  ",width=15,height=2,command=(lambda n=6: addnum(n)))
+b6.grid(row=3, column=0)
+
 bq = tk.Button(frame, text = "  Close  ",width=15,height=2,command = quit)
-bq.grid(row=3, column=0)
-badd = tk.Button(frame, text = "  +  ",width=15,height=2,command=(lambda opr='+': savenum()))
-badd.grid(row=2, column=2)
+bq.grid(row=4, column=0)
+badd = tk.Button(frame, text = "  +  ",width=15,height=2,command=(lambda op='+' : savenum(op)))
+badd.grid(row=2, column=3)
+bequ = tk.Button(frame, text = "  =  ",width=15,height=2,command=calc)
+bequ.grid(row=3, column=3)
 
 win.mainloop()
-
